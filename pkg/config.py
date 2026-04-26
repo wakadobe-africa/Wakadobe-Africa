48ba3fe9ef2b1a3ce29e4386f8cc64e141b86399
Folarin Kayode <forlarinkayode@gmail.com>
2026-04-26 22:21:37 +0100

    Set DEBUG=False for production

--- pkg/config.py
+++ pkg/config.py
@@ -7,6 +7,8 @@ class General(object):
     APP_NAME='wakadobeblog'
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    DEBUG = False
+    TESTING = False
     SQLALCHEMY_TRACK_MODIFICATION=False
     RATELIMIT_DEFAULT = '120 per hour'
     RATELIMIT_STORAGE_URI = 'memory://'
