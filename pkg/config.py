d10f18e976758905cfebe2bbf32fe59405396a4d
Folarin Kayode <forlarinkayode@gmail.com>
2026-04-26 20:43:47 +0100

    Production set up

--- pkg/config.py
+++ pkg/config.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class General(object):
     APP_NAME='wakadobeblog'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATION=False
     RATELIMIT_DEFAULT = '120 per hour'
     RATELIMIT_STORAGE_URI = 'memory://'
     RATELIMIT_HEADERS_ENABLED = True
+
 class LiveConfig(General):
     DATABASE = 'wakadobedb'
     
