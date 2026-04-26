33d687a0df38f8e004b7f3b4d31e08d7c73aac78
Folarin Kayode <forlarinkayode@gmail.com>
2026-04-26 22:44:56 +0100

    Deployment set up

--- pkg/config.py
+++ pkg/config.py
@@ -8,7 +8,7 @@ class General(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     DEBUG = False
-    TESTING = False
+    
     SQLALCHEMY_TRACK_MODIFICATION=False
     RATELIMIT_DEFAULT = '120 per hour'
     RATELIMIT_STORAGE_URI = 'memory://'
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from pkg import app
+from run import app
 
 if __name__ == "__main__":
     app.run()
