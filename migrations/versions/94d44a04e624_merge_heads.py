fc2824835850ab82e005f13e5ac88ed9737ca753
Folarin Kayode <forlarinkayode@gmail.com>
2026-04-26 23:45:52 +0100

    merge migrations head

---
+++ migrations/versions/94d44a04e624_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 94d44a04e624
+Revises: a2c4f6e8b9d1, d3f91b7c4a2e
+Create Date: 2026-04-26 23:41:36.071089
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '94d44a04e624'
+down_revision = ('a2c4f6e8b9d1', 'd3f91b7c4a2e')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
