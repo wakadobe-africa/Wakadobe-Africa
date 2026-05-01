90421e059d607af61c1184199d967f87ee87ec27
Folarin Kayode <forlarinkayode@gmail.com>
2026-05-01 11:43:12 +0100

    upload image configure

---
+++ pkg/upload.py
@@ -0,0 +1,10 @@
+import cloudinary
+from cloudinary.uploader import upload
+import os
+
+cloudinary.config( 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+  api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+  api_secret = os.environ.get('CLOUDINARY_API_SECRET'),
+  secure = True
+)
